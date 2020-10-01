f64670674a3f1128ec987f4fb7fa0605335b2341
orhanors <orsorhan1@gmail.com>
2020-10-01 16:42:53 +0300

    Update M1-D4.js
    all completed

--- M1-D4.js
+++ M1-D4.js
@@ -16,10 +16,7 @@ const area = (l1, l2) => (l1 + l2) * 2;
 Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
 */
 
-const crazySum = function (int1, int2) {
-    let sumofTriple = int1 === int2 ? (int1 + int2) * 3 : "Values aren't same";
-    return sumofTriple;
-}
+const crazySum = (int1, int2) => int1 === int2 ? (int1 + int2) * 3 : "Values aren't same";
 
 
 /* EXERCISE 3
@@ -41,49 +38,70 @@ const crazyDiff = function (number) {
 Write a function "boundary" which accept an integer N and returns true if N is within 20 and 100 (included) or equal to 400.
 */
 
-const boundary = function (N) {
-    let whatIsN = (20 <= N <= 200 || N === 400) ? true : false;
-    return whatIsN;
-}
+const boundary = N => (20 <= N <= 200 || N === 400) ? true : false;
 
-/* WRITE YOUR CODE HERE */
+
 
 /* EXERCISE 5
 Write a function "strivify" which accepts a string S. Add to S "Strive" in front of a given string, if the given string begins with "Strive" then return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+const strivift = S => (S.toLowerCase() === "Strive".toLowerCase()) ? S : "Strive" + S
+
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
-/* WRITE YOUR CODE HERE */
+const check3and7 = N => (N % 3 === 0 || N % 7 === 0) ? true : false;
+
+
 
 /* EXERCISE 7
 Write a function "reverseString" to reverse programmatically a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR CODE HERE */
+const reverseString = S => S.split("").reverse().join("");
+
+
 
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as parameter
 */
 
-/* WRITE YOUR CODE HERE */
+const upperFirst = function (S) {
+    const splittedString = S.split(" ");
+
+    for (let i = 0; i < splittedString.length; i++) {
+
+        splittedString[i] = splittedString[i].charAt(0).toUpperCase() + splittedString[i].substring(1);
+    }
+    return splittedString.join(" ")
+}
 
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
 
-/* WRITE YOUR CODE HERE */
+const cutString = S => S.slice(1, S.length - 1)
+
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10
 */
 
-/* WRITE YOUR CODE HERE */
+const giveMeRandom = function (n) {
+
+    n = n > 10 ? 10 : n
+
+    const newArr = []
+    for (let i = 0; i < n; i++) {
+        newArr.push(Math.floor(Math.random() * Math.floor(n)))
+    }
+    return newArr
+}
+
 
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository and share the link to your commit with your tutor.
